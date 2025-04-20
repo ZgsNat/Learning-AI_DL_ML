63060a1426efff36c6e85ba56184c31ed9348615
ZgsNat <nguyenthaithinh9876@gmail.com>
2025-04-21 01:23:52 +0700

    export to html

--- Learning ML (Prediction and Classification)/classification_model.py
+++ Learning ML (Prediction and Classification)/classification_model.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from ydata_profiling import ProfileReport
 
 data = pd.read_csv("diabetes.csv")
-print(data) 
+# print(data.describe())
+# print(data.info())
+# print(data.corr())
+profile = ProfileReport(data, title="Pandas Profiling Report", explorative=True)
+# profile.to_file("diabetes.html")
