38a0f47cd4edd25ebf4db9dba70369546098ab10
ZgsNat <nguyenthaithinh9876@gmail.com>
2025-04-21 20:56:46 +0700

    using data wrangle

--- Learning ML (Prediction and Classification)/classification_model.py
+++ Learning ML (Prediction and Classification)/classification_model.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from ydata_profiling import ProfileReport
-
-data = pd.read_csv("diabetes.csv")
+import os
+# Get the current file's directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct path to data file
+data_path = os.path.join(current_dir, "diabetes.csv")
+data = pd.read_csv(data_path)
+# data = pd.read_csv("diabetes.csv")
 # print(data.describe())
 # print(data.info())
 # print(data.corr())
-profile = ProfileReport(data, title="Pandas Profiling Report", explorative=True)
+# profile = ProfileReport(data, title="Pandas Profiling Report", explorative=True)
 # profile.to_file("diabetes.html")
+target = "Outcome"
+x = data.drop(target, axis=1)
+y = data[target]
